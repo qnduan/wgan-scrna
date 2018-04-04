7042c65385040ba5277583fd9dad799e4f4908af
EC2 Default User <ec2-user@ip-172-16-63-26.us-west-2.compute.internal>
2018-04-04 22:53:11 +0000

    best 2

--- rna-seq/model.py
+++ rna-seq/model.py
@@ -9,13 +9,13 @@ class DisNet(nn.Module):
         super(DisNet, self).__init__()
         self.dim_in = dim_in
 
-        self.fc1 = nn.Linear(dim_in,200)
-        self.fc2 = nn.Linear(200,200)
-        self.fc3 = nn.Linear(200,1)
+        self.fc1 = nn.Linear(dim_in,600)
+        self.fc2 = nn.Linear(600,600)
+        self.fc3 = nn.Linear(600,1)
 
     def forward(self,x):
-        x = F.leaky_relu(self.fc1(x),negative_slope=0.2)
-        x = F.leaky_relu(self.fc2(x),negative_slope=0.2)
+        x = F.leaky_relu(self.fc1(x))
+        x = F.leaky_relu(self.fc2(x))
         x = self.fc3(x)
         return x
 
@@ -25,12 +25,13 @@ class GenNet(nn.Module):
         super(GenNet, self).__init__()
         self.dim_in = dim_in
 
-        self.fc1 = nn.Linear(dim_in,600)
-        self.fc2 = nn.Linear(600,600)
-        self.fc3 = nn.Linear(600,dim_out)
+        self.fc1 = nn.Linear(dim_in,1200)
+        self.fc2 = nn.Linear(1200,1200)
+        self.fc3 = nn.Linear(1200,dim_out)
 
     def forward(self,x):
-        x = F.leaky_relu(self.fc1(x),negative_slope=0.2)
-        x = F.leaky_relu(self.fc2(x),negative_slope=0.2)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        # change to leaky?
         x = F.relu(self.fc3(x))
         return x
