0d21e82d62c842e13eb367ca3e2249c07fe1b233
EC2 Default User <ec2-user@ip-172-16-63-26.us-west-2.compute.internal>
2018-07-13 23:42:58 +0000

    up

--- rna-seq/model.py
+++ rna-seq/model.py
@@ -12,14 +12,14 @@ class DisNet(nn.Module):
         self.fc1 = nn.Linear(dim_in,200)
 #         self.dp1 = nn.Dropout()
 #         self.fc2 = nn.Linear(200,200)
-        self.dp2 = nn.Dropout()
+#         self.dp2 = nn.Dropout()
         self.fc3 = nn.Linear(200,1)
 
     def forward(self,x):
         x = F.leaky_relu(self.fc1(x))
 #         x = self.dp1(x)
 #         x = F.leaky_relu(self.fc2(x))
-        x = self.dp2(x)
+#         x = self.dp2(x)
         x = self.fc3(x)
         return x
 
@@ -32,14 +32,14 @@ class GenNet(nn.Module):
         self.fc1 = nn.Linear(dim_in,600)
 #         self.dp1 = nn.Dropout()
 #         self.fc2 = nn.Linear(600,600)
-        self.dp2 = nn.Dropout()
+#         self.dp2 = nn.Dropout()
         self.fc3 = nn.Linear(600,dim_out)
 
     def forward(self,x):
         x = F.relu(self.fc1(x))
 #         x = self.dp1(x)
 #         x = F.relu(self.fc2(x))
-        x = self.dp2(x)
+#         x = self.dp2(x)
         # change to leaky?
         x = F.relu(self.fc3(x))
         return x
