2a3a2165d6b31bd1947cb79eee9ec9b2d21a0277
frlender <>
2018-07-13 11:39:25 -0700

    update

--- rna-seq/explore.py
+++ rna-seq/explore.py
@@ -1,15 +1,3 @@
-from sampler import mat, sample_x, sample_z
-import plot
-import pylab
-
-zs = sample_z(50,100)
-pylab.hist(pylab.ravel(zs),bins=100)
-
-pylab.hist(pylab.ravel(mat[:,:50]),bins=100)
-
-pylab.hist(pylab.ravel(samples_fake_numpy),bins=100)
-
-
 
 #plot.plot_corr(mat[:,:100].T)
 #
@@ -18,3 +6,11 @@ pylab.hist(pylab.ravel(samples_fake_numpy),bins=100)
 #plot.plot_scatter(m1.T,m2.T)
 #
 #cc = sample_x(32)
+
+import qn
+
+mat = qn.load('data/ipf_log_tpm.pkl')
+import pandas as pd
+x = pd.DataFrame(mat)
+
+feather.write_dataframe(x,'~/Documents/single-cell-seq/explore/gan/mat_6788.ft')
