8f75191ac17b74c5e1b3a936d6ffe4c0d627a6e0
qnduan <geonann@163.com>
2018-08-27 16:02:34 -0700

    ignore big

--- rna-seq/explore.py
+++ rna-seq/explore.py
@@ -13,4 +13,27 @@ mat = qn.load('data/ipf_log_tpm.pkl')
 import pandas as pd
 x = pd.DataFrame(mat)
 
-feather.write_dataframe(x,'~/Documents/single-cell-seq/explore/gan/mat_6788.ft')
+feather.write_dataframe(x,'/Users/qiaonan/Documents/single-cell-seq/explore/gan/mat_6788.ft')
+
+
+import feather
+import numpy as np
+from scipy.spatial.distance import pdist
+import torch
+from model_extract import DisNet, GenNet
+from torch import nn
+from torch.autograd import Variable
+import qn
+import pandas as pd
+
+dis_net = DisNet(6982)
+dis_params = torch.load('temp_models/dis_net_cpu_10000.pt')
+dis_net.load_state_dict(dis_params)
+
+x_ = x.values.T.astype('float32')
+y1, y2 = dis_net.forward(torch.from_numpy(x_))
+features = y1.data.numpy()
+f = pd.DataFrame(features.T)
+
+path = '/Users/qiaonan/Documents/single-cell-seq/explore2/gan'
+feather.write_dataframe(f,path+'/pollen_200.ft')
